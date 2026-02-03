65344080df39d76ed4ebb12de9b35e410823c7e6
lncesht <lanceimperial402@gmail.com>
2026-02-04 06:43:26 +0800

    Configure for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Header from './components/Header';
 import Hero from './components/Hero';
